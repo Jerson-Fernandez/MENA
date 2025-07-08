d41a0e4a70d2b3906ee1b08e882c34cff27dd594
Jerson <125468052+Jerson-Fernandez@users.noreply.github.com>
2025-07-08 10:14:44 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'control-pagos-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activar Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si encuentra en cache, devuelve la respuesta cacheada
+        if (response) {
+          return response;
+        }
+        // Si no, hace la petición a la red
+        return fetch(event.request);
+      })
+      .catch(() => {
+        // Si falla todo, devuelve la página principal
+        return caches.match('/index.html');
+      })
+  );
+});
